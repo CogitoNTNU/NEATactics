66c9c98ee9f0f94c02ba1a4d5e09c94c7aefee5c
Vetlets05 <vetlets@icloud.com>
2024-09-03 19:53:51 +0200

    feat: Add connection class

---
+++ src/test_nodes.py
@@ -0,0 +1,8 @@
+class Connections:
+    def __init__(self, in_node, out_node, weight, is_enabeled, innovation_number):
+        self.in_node = in_node
+        self.out_node = out_node
+        self.weight = weight
+        self.is_enabeled = is_enabeled
+        self.innovation_number = innovation_number
+    
