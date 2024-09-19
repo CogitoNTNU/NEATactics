39467b799ccd215cd166545baa137e7aa1d9d42e
Kacper Pawlowski <kacperp@stud.ntnu.no>
2024-09-19 19:01:06 +0200

    cleaned visualize genome test

--- test/visualize_genome_test.py
+++ test/visualize_genome_test.py
@@ -2,9 +2,8 @@ from src.visualize_genome import visualize_genome
 from src.nodes import Genome, Node, ConnectionGene
 import random
 
-def test_visualize_genome():
+def generate_nodes():
     list_of_nodes = []
-    list_of_connections = []
     for i in range(200):
         color = random.random()
         list_of_nodes.append(Node(i, "Input", color))
@@ -14,13 +13,22 @@ def test_visualize_genome():
     for i in range(202, 207):
         color = random.random()
         list_of_nodes.append(Node(i, "Output", color))
+    return list_of_nodes
+
+def create_connections(list_of_nodes):
+    list_of_connections = []
     for i in range(200):
         for j in range(200, 202):
             list_of_connections.append(ConnectionGene(list_of_nodes[i], list_of_nodes[j], 1, True, 1))
     for i in range(202, 207):
         for j in range(200, 202):
             list_of_connections.append(ConnectionGene(list_of_nodes[j], list_of_nodes[i], 1, True, 1))
+    return list_of_connections
 
+def test_visualize_genome():
+    list_of_nodes = generate_nodes()
+    list_of_connections = create_connections(list_of_nodes)
+    
     genome = Genome(1)
     for i in list_of_nodes:
         genome.add_node(i)
