f81d454b1ce9e8a0b8f3b54fa09eea933fd338f7
Christian Fredrik Johnsen <christian@johnsen.no>
2024-09-24 18:13:51 +0200

    refactor: use only_right movement in test_gym_environment.

--- test/mario_rendering_test.py
+++ test/mario_rendering_test.py
@@ -1,18 +1,18 @@
 from src.environments.mario_env import MarioJoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import RIGHT_ONLY
 
 def test_gym_environment():
     ENV_NAME = "SuperMarioBros-v0"
 
     env = gym_super_mario_bros.make(ENV_NAME)
-    env = MarioJoypadSpace(env, SIMPLE_MOVEMENT)
+    env = MarioJoypadSpace(env, RIGHT_ONLY)
     env.metadata['render_modes'] = "rgb_array"
     env.metadata['render_fps'] = 60
     env.reset()
 
     for _ in range(100):
-        action = SIMPLE_MOVEMENT.index(["right"])
+        action = RIGHT_ONLY.index(["right"])
         sr = env.step(action) # State, Reward, Done, Info
         
         if sr.done:
