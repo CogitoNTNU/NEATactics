6352c4fe20b962858a31bba04398fb2eb24cdb36
Christian Fredrik Johnsen <christian@johnsen.no>
2024-10-10 08:49:19 +0200

    refactor: Heavy refactorizaion of visualization_position_utils, much easier to handle now. Relevant constants are placed in viz_config.py

--- src/visualization/visualization_position_utils.py
+++ src/visualization/visualization_position_utils.py
@@ -1,6 +1,9 @@
 import random
 from typing import List, Dict, Tuple
-from src.visualization.viz_config import HORIZONTAL_GAP_INPUT, VERTICAL_GAP, LAYER_GAP_OUTPUT, BOX_HEIGHT
+from src.visualization.viz_config import HORIZONTAL_GAP_INPUT, VERTICAL_GAP, NUM_INPUT_COLS, NUM_INPUT_ROWS, XSTART_INPUT, YSTART, XSTART_OUTPUT
+
+INPUT_HEIGHT = NUM_INPUT_ROWS * VERTICAL_GAP
+INPUT_XEND = XSTART_INPUT + HORIZONTAL_GAP_INPUT * (NUM_INPUT_COLS - 1)
 
 def get_position_dict(layers: List[List[int]]) -> Dict[int, Tuple[float, float]]:
     """
@@ -17,33 +20,31 @@ def get_position_dict(layers: List[List[int]]) -> Dict[int, Tuple[float, float]]
         if layer_idx == 0:
             get_input_pos(pos, layer)
         elif layer_idx == total_layers - 1:
-            get_output_pos(pos, layer, total_layers)
+            get_output_pos(pos, layer)
         else:
             get_hidden_pos(pos, layer)
     return pos
 
 def get_input_pos(pos: Dict[int, Tuple[float, float]], layer: List[int]):
-    x_pos = 0  # Input layer starts at the far left
-    for i, node in enumerate(layer[:-1]): # Organize the first layer into a 20x10 grid, starting from top-left (0,0)
-        row = i // 20  # There are 10 rows, so row is determined by i // 20
-        col = i % 20  # Columns are determined by i % 20
-        pos[node] = (x_pos + col * HORIZONTAL_GAP_INPUT, -row * VERTICAL_GAP)  # Adjust x (columns) and y (rows)
-    pos[layer[-1]] = ((x_pos + 19 * HORIZONTAL_GAP_INPUT), -10 * VERTICAL_GAP) # bias node
+    """Provide positions to all input nodes. These input nodes consist of the state (which is a frame of the mario game) + a bias node."""
+    for i, node in enumerate(layer[:-1:]): # Organize the first layer into a 20x10 grid, starting from top-left (0,0)
+        row = i // NUM_INPUT_COLS  # There are 10 rows, so row is determined by i // 20
+        col = i % NUM_INPUT_COLS  # Columns are determined by i % 20
+        pos[node] = (XSTART_INPUT + col * HORIZONTAL_GAP_INPUT, YSTART + ((NUM_INPUT_ROWS - 1) - row) * VERTICAL_GAP)  # Adjust x (columns) and y (rows)
+    pos[layer[-1]] = (INPUT_XEND, YSTART + NUM_INPUT_ROWS * VERTICAL_GAP) # bias node
 
 def get_hidden_pos(pos: Dict[int, Tuple[float, float]], layer: List[int]):
-    # Hidden layers are placed regularly between the input and output layers
-    y_start = -(len(layer) - 1) * VERTICAL_GAP / 2  # Center the layer vertically
-    for i, node in enumerate(layer):
-        x_pos = round(random.uniform(10.5, 14.5), 2)
-        y_pos = round(random.uniform(0, 18), 2)
-        pos[node] = (x_pos, -y_pos)  # Place nodes vertically
+    """Hidden nodes are currently being placed randomly within a pre-defined square box in the graph"""
+    for node in layer:
+        x_pos = round(random.uniform(INPUT_XEND + 0.5, XSTART_OUTPUT - 0.5), 2)
+        y_pos = round(random.uniform(YSTART, YSTART + INPUT_HEIGHT), 2)
+        pos[node] = (x_pos, y_pos)
     
-def get_output_pos(pos: Dict[int, Tuple[float, float]], layer: List[int], total_layers: int):
-    x_pos = total_layers * LAYER_GAP_OUTPUT   # Place output nodes at the farthest right
-    y_gap = (BOX_HEIGHT) / (len(layer))  # Center the output layer vertically
-    
+def get_output_pos(pos: Dict[int, Tuple[float, float]], layer: List[int]):
+    """Get positions for the output nodes. Each output node represents an action which can be taken in the environment. EXAMPLE: GO_RIGHT"""
+    y_gap = NUM_INPUT_ROWS * VERTICAL_GAP / len(layer)
     for i, node in enumerate(layer):
-        y_pos = - y_gap * i - y_gap/2
-        pos[node] = (x_pos, y_pos)  # Place nodes vertically
+        y_pos = YSTART + y_gap * i
+        pos[node] = (XSTART_OUTPUT, y_pos)  # Place nodes vertically
 
 
